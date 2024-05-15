77fc14ac13fb43ff0c1050db0b1332b7f494783d
crashrebootrepeat <bsod@crashrebootrepeat.com>
2024-05-15 10:29:50 -0400

    Add files via upload

---
+++ osdzti.ps1
@@ -0,0 +1,24 @@
+﻿Write-Host  -ForegroundColor Cyan "Starting OSD Cloud for Helient"
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating The OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+#Start OSDCloud ZTI the RIGHT way
+Write-Host  -ForegroundColor Cyan "Running OSD Cloud with ZTI"
+Start-OSDCloud -OSNae "Windows 11 23H2 x64" -OSLanguage en-us -OSEdition Enterprise -OSActivation volume -ZTI
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
