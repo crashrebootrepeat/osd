3ba9b7ba28f271071fcf8f3ac45f9591b359be63
crashrebootrepeat <bsod@crashrebootrepeat.com>
2024-05-28 22:18:31 -0400

    Update oobetasks.ps1

--- oobetasks.ps1
+++ oobetasks.ps1
@@ -1,27 +1,27 @@
-$scriptFolderPath = "$env:SystemDrive\OSDCloud\Scripts"
-$ScriptPathSendKeys = $(Join-Path -Path $scriptFolderPath -ChildPath "SendKeys.ps1")
-$ScriptPathInstallAPC = $(Join-Path -Path $scriptFolderPath -ChildPath "instapc.ps1")
-$ScriptPathRunAPC = $(Join-Path -Path $scriptFolderPath -ChildPath "runapc.ps1")
+# oobetasks.osdcloud.ch
 
+$scriptFolderPath = "$env:SystemDrive\OSDCloud\Scripts"
+$ScriptPathOOBE = $(Join-Path -Path $scriptFolderPath -ChildPath "OOBE.ps1")
+$ScriptPathSendKeys = $(Join-Path -Path $scriptFolderPath -ChildPath "SendKeys.ps1")
 
 If(!(Test-Path -Path $scriptFolderPath)) {
     New-Item -Path $scriptFolderPath -ItemType Directory -Force | Out-Null
 }
 
-$instapc =@"
-`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-instapc.log"
+$OOBEScript =@"
+`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
 Write-Host -ForegroundColor DarkGray "NUGET Package Provider"
 Start-Process PowerShell -ArgumentList "-NoL -C Install-PackageProvider -Name NuGet -Force -Verbose" -Wait
 
 Write-Host -ForegroundColor DarkGray "Installing Get-WindowsAutoPilotInfoCommunity"
-Start-Process PowerShell -ArgumentList "-NoL -C Install-Script Get-WindowsAutoPilotInfoCommunity -Force -Verbose" -Wait
+Start-Process PowerShell -ArgumentList "-NoL -C Install-Module Get-WindowsAutoPilotInfoCommunity -Force -Verbose" -Wait
 
 Stop-Transcript -Verbose | Out-File
 "@
 
-Out-File -FilePath $ScriptPathInstallAPC -InputObject $instapc -Encoding ascii
+Out-File -FilePath $ScriptPathOOBE -InputObject $OOBEScript -Encoding ascii
 
 $SendKeysScript = @"
 `$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-SendKeys.log"
@@ -43,17 +43,7 @@ Write-Host -ForegroundColor DarkGray "SendKeys: SHIFT + F10"
 Stop-Transcript -Verbose | Out-File
 "@
 
-$runapc =@"
-`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-runapc.log"
-Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
-
-Write-Host -ForegroundColor DarkGray "Running Get-WindowsAutoPilotInfoCommunity"
-Start-Process PowerShell -ArgumentList "-NoL -C Get-WindowsAutoPilotInfoCommunity -online"
-
-Stop-Transcript -Verbose | Out-File
-"@
-
-Out-File -FilePath $ScriptPathRunAPC -InputObject $SendKeysScript -Encoding ascii
+Out-File -FilePath $ScriptPathSendKeys -InputObject $SendKeysScript -Encoding ascii
 
 # Download ServiceUI.exe
 Write-Host -ForegroundColor Gray "Download ServiceUI.exe from GitHub Repo"
@@ -83,7 +73,7 @@ $taskFolder = $ShedService.GetFolder("\")
 # https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
 $taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
 
-# Create Scheduled Task to Install AutoPilotInfo
+# Create Scheduled Task for OSDCloud post installation with 20 seconds delay
 $TaskName = "Scheduled Task for OSDCloud post installation"
 
 $ShedService = New-Object -comobject 'Schedule.Service'
@@ -101,31 +91,7 @@ $trigger.Enabled = $true
 
 $action = $Task.Actions.Create(0)
 $action.Path = 'C:\OSDCloud\ServiceUI.exe'
-$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathInstallAPC + ' -NoExit'
-
-$taskFolder = $ShedService.GetFolder("\")
-# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
-$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
-
-# Create Scheduled Task to run AutoPilotINfo
-$TaskName = "Scheduled Task for OSDCloud post installation"
-
-$ShedService = New-Object -comobject 'Schedule.Service'
-$ShedService.Connect()
-
-$Task = $ShedService.NewTask(0)
-$Task.RegistrationInfo.Description = $taskName
-$Task.Settings.Enabled = $true
-$Task.Settings.AllowDemandStart = $true
-
-# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
-$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
-$trigger.Delay = 'PT20S'
-$trigger.Enabled = $true
-
-$action = $Task.Actions.Create(0)
-$action.Path = 'C:\OSDCloud\ServiceUI.exe'
-$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathRunAPC + ' -NoExit'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathOOBE + ' -NoExit'
 
 $taskFolder = $ShedService.GetFolder("\")
 # https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
