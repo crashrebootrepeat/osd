a424c9c2afc1a87cf62f3f8e1fb34c3f5f144107
crashrebootrepeat <bsod@crashrebootrepeat.com>
2024-05-29 11:18:20 -0400

    Add files via upload

---
+++ InstallAPInfo.ps1
@@ -0,0 +1,16 @@
+﻿$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-InstallAPInfo.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
+Write-Host -ForegroundColor Green "Install NuGet"
+
+Start-Sleep -Seconds 5
+
+Install-PackageProvider -Name NUGET -Force
+
+Start-Sleep -Seconds 5
+
+Write-Host -ForegroundColor Green "Install NuGet"
+
+Install-Script -Name get-windowsautopilotinfocommunity -Force
+
+Stop-Transcript
