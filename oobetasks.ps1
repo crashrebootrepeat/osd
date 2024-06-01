460401efb012947a68ec6299658199853f329efa
crashrebootrepeat <bsod@crashrebootrepeat.com>
2024-06-01 00:35:20 -0400

    Update oobetasks.ps1

--- oobetasks.ps1
+++ oobetasks.ps1
@@ -11,7 +11,7 @@ $OOBEScript =@"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
 Write-Host -ForegroundColor DarkGray "Starting PowerShell and Setting Directory"
-Start-Process PowerShell | Set-Location "C:\ProgramData\PCConfig\Autopilot"
+Start-Process --WorkingDirectory "C:\ProgramData\PCConfig\Autopilot" PowerShell.exe -NoNewWindow
 
 Stop-Transcript -Verbose | Out-File
 "@
